0503b463dd99930cc1feba80e6b348962e210775
platisam <platisam@gmail.com>
2023-09-13 14:07:59 +0200

    creating showAlert function for displaying alert

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
     e.preventDefault();
     if (!name) {
       // display alert
+      //setAlert({ show: true, msg: "please enter value", type: "danger" });
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // deal with edit
     } else {
@@ -27,10 +29,14 @@ function App() {
     }
   };
 
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
