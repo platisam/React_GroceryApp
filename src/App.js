3671608f8233df1c219b26721493ed9b5d637f3f
platisam <platisam@gmail.com>
2023-09-13 12:53:09 +0200

    setting form and adding Alert component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import List from "./List";
+import Alert from "./Alert";
 
 function App() {
   const [name, setName] = useState("");
@@ -8,10 +9,32 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
   return (
     <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
       <div className="grocery-container">
         <List />
+        <button className="clear-btn">clear items</button>
       </div>
     </section>
   );
