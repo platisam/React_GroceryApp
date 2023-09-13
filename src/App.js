b1a5f441cf18bc814b5f506027dd55236f27287e
platisam <platisam@gmail.com>
2023-09-13 12:35:08 +0200

    setting states and List component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React, { useState } from "react";
+import List from "./List";
+
 function App() {
-  return <h2>Grocery App</h2>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  return (
+    <section className="section-center">
+      <div className="grocery-container">
+        <List />
+      </div>
+    </section>
+  );
 }
 
 export default App;
